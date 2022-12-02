d4cc6e49c5bceda2e70b3594262193ee7d5afe30
Christopher2212 <chrisgx21@gmail.com>
2022-12-01 19:26:47 -0800

    Added Init login

---
+++ defines.php
@@ -0,0 +1,6 @@
+<?php
+    session_start();
+    define('FACEBOOK_APP_ID','717824432637782');
+    define('FACEBOOK_APP_SECRET','●●●●●●●●');
+    define('FACEBOOK_APP_REDIRECT_URI','https://christopher2212.github.io/SecInfoFinal/GetInfo.php');
+?>
---
+++ obtaining_access_token.php
@@ -0,0 +1,34 @@
+<php?
+    include 'defines.php';
+
+    // load graph-sdk files
+    require_once __DIR__ . 'php-graph-sdk-5.x\src\Facebook\autoload.php'
+    
+    // facebook credentials array
+
+    $creds = array(
+        'app_id' => FACEBOOK_APP_ID,
+        'app_secret' => FACEBOOK_APP_SECRET,
+        'default_graph_version' => 'v5'
+        'persistent_data_handeler' => 'session'
+    );
+
+    // create facebook object
+    
+    $facebook = new Facebook\Facebook($creds);
+
+    $helper = $facebook -> getRedirectLoginHelper();
+
+    $oAuth2Client = $facebook -> getOAuth2Client(); 
+
+    if(isset($_GET['code'])){
+
+    }else{
+        $perissions = ['public_profile', 'instagram_basic', 'pages_show_list'];
+        $loginUrl  = $helper -> getLoginUrl(FACEBOOK_APP_REDIRECT_URL, $permissions)
+        
+        echo'<a href="' . loginUrl . '">
+            Login With Facebook
+            </a>';
+    }
+?>

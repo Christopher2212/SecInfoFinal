56adf46f426646fdcca2675c1ba754a8c339eda3
Christopher2212 <chrisgx21@gmail.com>
2022-12-03 14:15:33 -0800

    Little Tidbit

--- defines.php
+++ defines.php
@@ -2,7 +2,7 @@
     session_start();
     define('FACEBOOK_APP_ID','717824432637782');
     define('FACEBOOK_APP_SECRET','●●●●●●●●');
-    define('FACEBOOK_APP_REDIRECT_URI','https://christopher2212.github.io/SecInfoFinal/GetInfo.php');
+    define('FACEBOOK_APP_REDIRECT_URI','http://secinfofinal.com/insta_info');
     define('ENDPOINT_BASE', 'https://graph.facebook.com/v15.0/');
 
     // accessToken

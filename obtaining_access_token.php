92c8e5db93b75dceaa504262f3230f387a3a4062
Marcus McClatchy <marcusmc10101@gmail.com>
2022-12-02 23:28:15 -0800

    New Setup of access token

--- obtaining_access_token.php
+++ obtaining_access_token.php
@@ -1,7 +1,5 @@
 <?php
 
-    use Facebook\Facebook;
-
     include 'defines.php';
 
     // load graph-sdk files
@@ -27,13 +25,33 @@
     if(isset($_GET['code'])){ // get access token
         try {
             $accessToken = $helper->getAccessToken();
-        } catch (php-graph-sdk-5.x\src\Facebook\Exceptions\FacebookResponseException.php $e){
-            echo 'Graph returned an error ' . $e->getMessage;
+        } catch (Facebook\Exceptions\FacebookResponseException $e){
+            echo 'Graph returned an error ' . $e->getMessage();
+        } catch (Facebook\Exceptions\FacebookSDKException $e) {
+            echo 'Facebook SDK returned an error ' . $e->getMessage();
         }
+
+        echo '<h1>Short Lived Access Token</h1>';
+        print_r((string) $accessToken);
+
+        if ( !$accessToken->isLongLived()) { // exchange short for long
+            try {
+                $accessToken = $oAuth2Client->getLongLivedAccessToken( $accessToken );
+            } catch( Facebook\Exceptions\FacebookSDKException){
+                echo 'Error getting long lived access token ' . $e->getMessage();
+            }
+        }
+
+        echo '<pre>';
+        var_dump($accessToken);
+
+        $accessToken = (string) $accessToken;
+        echo '<h1>Long Lived Access Token</h1>';
+        print_r($accessToken);
 
     }else{
         $perissions = ['public_profile', 'instagram_basic', 'pages_show_list'];
-        $loginUrl  = $helper -> getLoginUrl(FACEBOOK_APP_REDIRECT_URL, $permissions);
+        $loginUrl  = $helper -> getLoginUrl(FACEBOOK_APP_REDIRECT_URI, $permissions);
         
         echo '<a href="' . $loginUrl . '">
             Login With Facebook

bf713c9f27cc6b5d7e7c025135cf2c07aefe048b
Christopher2212 <chrisgx21@gmail.com>
2022-12-03 13:33:28 -0800

    Almost there to Logging in

--- defines.php
+++ defines.php
@@ -2,5 +2,5 @@
     session_start();
     define('FACEBOOK_APP_ID','717824432637782');
     define('FACEBOOK_APP_SECRET','●●●●●●●●');
-    define('FACEBOOK_APP_REDIRECT_URI','https://localhost/SecInfoFinal/GetInfo.php');
+    define('FACEBOOK_APP_REDIRECT_URI','http://secinfofinal.com/insta_info');
 ?>

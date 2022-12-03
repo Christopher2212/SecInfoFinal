9578d6920cef917b7ccfc25a8292c2d98382aa36
Christopher2212 <chrisgx21@gmail.com>
2022-12-02 22:52:12 -0800

    Trimming

--- obtaining_access_token.php
+++ obtaining_access_token.php
@@ -27,7 +27,7 @@ use Facebook\Facebook;
     if(isset($_GET['code'])){ // get access token
         try {
             $accessToken = $helper->getAccessToken();
-        } catch (Facebook\Exceptions\FacebookResponseException $e){
+        } catch (php-graph-sdk-5.x\src\Facebook\Exceptions\FacebookResponseException.php $e){
             echo 'Graph returned an error ' . $e->getMessage;
         }
 
@@ -41,37 +41,3 @@ use Facebook\Facebook;
             
     }
 ?>
-
-
-
-
-// load graph-sdk files
-            require_once __DIR__ . 'php-graph-sdk-5.x\src\Facebook\autoload.php'
-            
-            // facebook credentials array
-
-            $creds = array(
-                'app_id' => FACEBOOK_APP_ID,
-                'app_secret' => FACEBOOK_APP_SECRET,
-                'default_graph_version' => 'v5'
-                'persistent_data_handeler' => 'session'
-            );
-
-            // create facebook object
-            
-            $facebook = new Facebook\Facebook($creds);
-
-            $helper = $facebook -> getRedirectLoginHelper();
-
-            $oAuth2Client = $facebook -> getOAuth2Client(); 
-
-            if(isset($_GET['code'])){
-
-            }else{
-                $perissions = ['public_profile', 'instagram_basic', 'pages_show_list'];
-                $loginUrl  = $helper -> getLoginUrl(FACEBOOK_APP_REDIRECT_URL, $permissions)
-                
-                echo'<a href="' . loginUrl . '">
-                    Login With Facebook
-                    </a>';
-            }
